d4eb0ed4c0384284cdca7cbf65920fc6acf40867
Adam <adam.garbo@gmail.com>
2023-06-29 10:24:15 +0200

    Improve code commenting

--- Software/Arduino/testing_code/MB7354_test/MB7354_test.ino
+++ Software/Arduino/testing_code/MB7354_test/MB7354_test.ino
@@ -1,52 +1,51 @@
 /*
+  Title:    MaxBotix MB7354 Test Code
+  Date:     June 27, 2023
+  Author:   Adam Garbo
 
-  MaxBotix MB7374 HRXL-MaxSonar-WRST7 Ultrasonic Precision Range Finder
-  https://www.maxbotix.com/Ultrasonic_Sensors/MB7374.htm
-
+  MaxBotix MB7354 HRXL-MaxSonar-WRS5
+  https://www.maxbotix.com/ultrasonic_sensors/mb7354.htm
+  
+  Wiring Diagram:
+  --------------------------------------------------
+  Colour    Pin    Description             Pin
+  --------------------------------------------------
+  White    1       Temperature Sensor      Not connected
+  Orange   2       Pulse Width Output      A3
+  Brown    3       Analog Voltage Output   Not connected
+  Green    4       Ranging Start/Stop      Not connected
+  Blue     5       Serial Output           Not connected
+  Red      6       Vcc                     5V
+  Black    7       GND                     GND
 */
 
-// MaxBotix MB7354 HRXL-MaxSonar-WRS5
-// https://www.maxbotix.com/ultrasonic_sensors/mb7354.htm
-// --------------------------------------------------
-// Colour    Pin    Description             Pin
-// --------------------------------------------------
-// White    1       Temperature Sensor      Not connected
-// Orange   2       Pulse Width Output      A3
-// Brown    3       Analog Voltage Output   Not connected
-// Green    4       Ranging Start/Stop      Not connected
-// Blue     5       Serial Output           Not connected
-// Red      6       Vcc                     5V
-// Black    7       GND                     GND
+#include <Statistic.h>
 
-#include <Statistic.h>          // https://github.com/RobTillaart/Arduino/tree/master/libraries/Statistic
+#define PIN_SNOW    A3  // Maxbotix pulse width output pin
+#define PIN_12V_EN  5   // 12 V step-up/down regulator
+#define PIN_5V_EN   6   // 5V step-down regulator
 
+Statistic snowDepthStatsAvg;  // Maxbotix average distances
+Statistic snowDepthStatsStd;  // Maxbotix std distances
+Statistic snowDepthStatsMax;  // Maxbotix max distances
+Statistic snowDepthStatsMin;  // Maxbotix min distances
+Statistic snowDepthStatsNan;  // Maxbotix nan samples
 
-#define PIN_SNOW            A3  // Maxbotix pulse width output pin
-#define PIN_12V_EN          5   // 12 V step-up/down regulator
-#define PIN_5V_EN           6   // 5V step-down regulator
-
-Statistic snowDepthStatsAvg;    // Maxbotix average distances
-Statistic snowDepthStatsStd;    // Maxbotix std distances
-Statistic snowDepthStatsMax;    // Maxbotix max distances
-Statistic snowDepthStatsMin;    // Maxbotix min distances
-Statistic snowDepthStatsNan;    // Maxbotix nan samples
-
-unsigned int  snowDepthAvg      = 0;      // Average distance from Maxbotix sensor to surface (mm)
-unsigned int  snowDepthStd      = 0;      // Standard deviation distance from Maxbotix sensor to surface (mm)
-unsigned int  snowDepthMax      = 0;      // Max distance from Maxbotix sensor to surface (mm)
-unsigned int  snowDepthMin      = 0;      // Min distance from Maxbotix sensor to surface (mm)
-unsigned int  snowDepthNan      = 0;      // Number of NaN readings in Maxbotix
+unsigned int snowDepthAvg = 0;  // Average distance from Maxbotix sensor to surface (mm)
+unsigned int snowDepthStd = 0;  // Standard deviation distance from Maxbotix sensor to surface (mm)
+unsigned int snowDepthMax = 0;  // Max distance from Maxbotix sensor to surface (mm)
+unsigned int snowDepthMin = 0;  // Min distance from Maxbotix sensor to surface (mm)
+unsigned int snowDepthNan = 0;  // Number of NaN readings in Maxbotix
 
 
-void setup() {
-  
-  // MaxBotix
+void setup()
+{
   pinMode(PIN_SNOW, INPUT);
   pinMode(PIN_5V_EN, OUTPUT);
   pinMode(PIN_12V_EN, OUTPUT);
   digitalWrite(PIN_5V_EN, LOW);   // Disable 5V power
   digitalWrite(PIN_12V_EN, LOW);  // Disable 12V power
-  
+
   Serial.begin(115200);
   while (!Serial);
 
@@ -54,13 +53,14 @@ void setup() {
   Serial.println("Mean\tStdev\tMax\tMin\tNan");
 }
 
-void loop() {
-  readMb7354(); // Read Maxbotix Distance sensor
+void loop()
+{
+  readMb7354();  // Read Maxbotix Distance sensor
   delay(5000);
 }
 
 // Read Maxbotix distance to surface
-void readMb7354() 
+void readMb7354()
 {
   // Wake sensor
   digitalWrite(PIN_5V_EN, HIGH);
@@ -70,20 +70,29 @@ void readMb7354()
   Statistic Maxbotix;
 
   // Create temporary variables
-  unsigned int z, zAvg, zStd, zMax, zMin, zNan;
+  unsigned int z = 0;
+  unsigned int zAvg = 0;
+  unsigned int zStd = 0;
+  unsigned int zMax = 0;
+  unsigned int zMin = 0;
+  unsigned int zNan = 0;
 
   // Get 30 z readings in mm, filtering out reading 50 mm
   // above/below sensor minumum/maximum readings
-  for (byte i = 0; i < 30; i++) {
-    z = pulseIn(PIN_SNOW, HIGH); // Read distance to snow surface
+  for (byte i = 0; i < 30; i++)
+  {
+    z = pulseIn(PIN_SNOW, HIGH);  // Read distance to snow surface
 
-    if (z > 550 && z < 4950) { // Filter readings
-      Maxbotix.add(z); // Add good readings to stats array
+    // Filter readings
+    if (z > 550 && z < 4950)
+    {
+      Maxbotix.add(z);   // Add good readings to stats array
     }
-    else {
-      zNan += 1; // Count bad readings
+    else
+    {
+      zNan += 1;  // Count bad readings
     }
-    delay(100); // Delay 0.1 secs between readings
+    delay(100);  // Delay 0.1 secs between readings
   }
 
   // Get stats from the Maxbotix array
@@ -94,16 +103,20 @@ void readMb7354()
 
   // Deal with issue of a maximum long number in the instance of no
   // readings within filtered range
-  if (zAvg > 5000) {
+  if (zAvg > 5000)
+  {
     zAvg = 0;
   }
-  if (zStd > 5000) {
+  if (zStd > 5000)
+  {
     zStd = 0;
   }
-  if (zMax > 5000) {
+  if (zMax > 5000)
+  {
     zMax = 0;
   }
-  if (zMin > 5000) {
+  if (zMin > 5000)
+  {
     zMin = 0;
   }
 
@@ -132,5 +145,4 @@ void readMb7354()
 
   // Sleep sensor
   digitalWrite(PIN_5V_EN, LOW);
-  delay(5000);
 }
